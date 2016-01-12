fc263e4521e8332d60329410cc502958da3cc562
steveoneil <steve@steveoneil.com>
2016-01-12 15:04:25 -0500

    Update Ex3 with gets

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,12 @@
+# Gets exercises
 puts "What is your name?"
+name = gets
+puts "Hi #{name}!"
+
+puts "I didn't get that. What is your name?"
+name = gets.chomp
+puts "Hi #{name}!"
+
+puts "Sorry...one more time. What is your name?"
+name = gets.strip
+puts "Hi #{name}!"

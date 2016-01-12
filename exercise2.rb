739f9d71307263bb55e04a1b8e504213363a2503
steveoneil <steve@steveoneil.com>
2016-01-12 14:06:54 -0500

    Add excercise 2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+# Calculating tip for $55 meal
+meal_cost = 55.00
+tip_percentage = 15.0
+
+tip = (tip_percentage / 100) * meal_cost
+
+puts "Tip amount for a $#{meal_cost} meal would be $#{tip}"
+
+tip_percentage = 20.0
+
+tip = (tip_percentage / 100) * meal_cost
+
+puts "A \"good\" tip amount for a $#{meal_cost} meal would be $#{tip}"

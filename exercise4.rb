b95de90d7208667156e119b31b78d907ca48ea78
steveoneil <steve@steveoneil.com>
2016-01-12 16:00:06 -0500

    Ex 4 with Bit, Maker & BitMaker entries

--- exercise4.rb
+++ exercise4.rb
@@ -3,7 +3,18 @@
 one_to_onehundred = (1..100)
 
 one_to_onehundred.each do |number|
+
   mod_3 = (number % 3 == 0)
   mod_5 = (number % 5 == 0)
-  puts mod_3.to_s + ", " + mod_5.to_s
+
+  if mod_3 && mod_5
+    puts "BitMaker"
+  elsif mod_3
+    puts "Bit"
+  elsif mod_5
+    puts "Maker"
+  else
+    puts number.to_s
+  end
+
 end
